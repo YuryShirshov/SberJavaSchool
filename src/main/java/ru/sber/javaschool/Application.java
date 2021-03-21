f11eb38cb04f76f51b95fff48f168508b2946585
Shirshov.Y.Y <Shirshov.Y.Y@sberbank.ru>
2021-03-21 20:39:30 +0300

    Init Commit

--- src/main/java/ru/sber/javaschool/Application.java
+++ src/main/java/ru/sber/javaschool/Application.java
@@ -2,19 +2,24 @@ package ru.sber.javaschool;
 
 import ru.sber.javaschool.lesson4.FileProcessor;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter source FileName: ");
+        System.out.print("Введите имя входного файла: ");
         String sourceFileName = scanner.nextLine();
-        System.out.print("Enter dest FileName:");
+        System.out.print("Введите имя выходного файла:");
         String destFileName = scanner.nextLine();
         FileProcessor fileProcessor = new FileProcessor();
-        fileProcessor.process(sourceFileName, destFileName);
+        try {
+            fileProcessor.process(sourceFileName, destFileName);
+            System.out.println("Обработка прошла успешно");
+        } catch (IOException e) {
+            System.out.println("Ошибка при обработке файла: " + e.getMessage());
+        }
+
     }
 }
